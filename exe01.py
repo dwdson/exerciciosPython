ee0a92742d227645efc232c3309a01937db9a52f
dwdson <dwdson@gmail.com>
2024-04-06 22:31:43 -0300

    commit de exe01 a 03

---
+++ exe01.py
@@ -0,0 +1,3 @@
+# Crie um programa que escreva "Olá mundo!"
+msg = 'Olá mundo'
+print(msg)
---
+++ exe03.py
@@ -0,0 +1,14 @@
+# recebe e some dois números inteiros
+n1 = int(input('Digite seu primeiro número: '))
+n2 = int(input('Digite seu segundo número: '))
+soma = n1 + n2
+print('A soma dos dois números vale: ', soma)
+#print(f'A soma dos dois números vale: {soma}')
+
+#print('A soma dos dois números vale:{} '.format(soma))
+
+#tipos primitivos
+#int: 7,-4,0,9875;
+#float: 4.5,0.076,15.223,7,0
+#boll: True, False
+#str: 'Olá','7,5',''
